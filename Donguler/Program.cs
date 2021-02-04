5c4626b56c9d2b870b42b6870f2d38cfe6d97166
Berke Karaman <berkekaraman99@hotmail.com>
2021-02-04 15:31:40 +0300

    20 Ocak Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            //string kurs2 = "Programlamaya Başlangıç için Temel Kurs";
+            //string kurs3 = "Java Kursu";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç için Temel Kurs", "Java Kursu","Python","C#","C++" };
+            
+            Console.WriteLine("Kurslar\n");
+            //for
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti\n");
+            //foreach dizi temelli tek tek dolaşan döngü
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
